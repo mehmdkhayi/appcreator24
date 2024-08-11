8e27a56688cdb5d72ce3b01abe58c4fa5273576a
mehmdkhayi <140708998+mehmdkhayi@users.noreply.github.com>
2024-08-11 20:43:58 +0100

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,13 +1,2 @@
-document.getElementById('html5').addEventListener('change', function() {
-      let elements = document.querySelectorAll('.element');
-      elements.forEach(function(element) {
-        element.style.display = 'none';
-      });
-      
-      let selectedValue = this.value;
-      let selectedElement = document.getElementById(selectedValue);
-      if (selectedElement) {
-        selectedElement.style.display = 'block';
-      }
-    });
+
 document.body.style.background ="red"
