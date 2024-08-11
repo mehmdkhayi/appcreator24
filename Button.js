c2d69c34de0f04e748ca05219880e5f2677e98ef
mehmdkhayi <140708998+mehmdkhayi@users.noreply.github.com>
2024-08-11 20:30:02 +0100

    Button.js

--- Button.js
+++ Button.js
@@ -1,3 +1,12 @@
-function add(){
-      document.body.style.background = 'black'
-    }
+document.getElementById('html5').addEventListener('change', function() {
+      let elements = document.querySelectorAll('.element');
+      elements.forEach(function(element) {
+        element.style.display = 'none';
+      });
+      
+      let selectedValue = this.value;
+      let selectedElement = document.getElementById(selectedValue);
+      if (selectedElement) {
+        selectedElement.style.display = 'block';
+      }
+    });
